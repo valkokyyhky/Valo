f6383e99f171e15cffeeab504c6823036329dc59
Valko <gracederousy@gmail.com>
2026-03-13 15:35:31 +0100

    Update brandtextloading/index.tsx

--- src/components/Loading/BrandTextLoading/index.tsx
+++ src/components/Loading/BrandTextLoading/index.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { BrandLoading } from '@lobehub/ui/brand';
 import CircleLoading from '../CircleLoading';
 import styles from './index.module.css';
 
@@ -14,12 +13,14 @@ const BrandTextLoading = ({ debugId }: BrandTextLoadingProps) => {
   return (
     <div className={styles.container}>
       <div aria-label="Loading" className={styles.brand} role="status">
-        {/* 方案A：如果 BrandLoading 支持直接传字符串 */}
-        <BrandLoading size={40} text="Valo" />
-        
-        {/* 方案B：如果上面的不生效，用这个组合方案 */}
-        {/* <CircleLoading />
-        <span style={{ marginLeft: 12, fontWeight: 600, fontSize: 16 }}>Valo</span> */}
+        <CircleLoading />
+        <span style={{ 
+          fontWeight: 600, 
+          fontSize: 16,
+          color: 'var(--brand-text-color)'
+        }}>
+          Valo
+        </span>
       </div>
 
       {showDebug && (
