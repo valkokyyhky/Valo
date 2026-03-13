20c4f7d75617ba306c087033adbdadca3b812394
Valko <gracederousy@gmail.com>
2026-03-13 06:53:18 +0100

    Update src/app/manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,16 +8,31 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     background_color: '#f8f8f8',
     theme_color: '#f8f8f8',
+
     icons: [
       {
         src: '/icons/icon-192x192.png',
         sizes: '192x192',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
+      },
+      {
+        src: '/icons/icon-192x192.maskable.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable'
       },
       {
         src: '/icons/icon-512x512.png',
         sizes: '512x512',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
+      },
+      {
+        src: '/icons/icon-512x512.maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
       }
     ]
   }
