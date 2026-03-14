d295034a134cb26dbd8cadca655289d5b05aef0b
Valko <gracederousy@gmail.com>
2026-03-14 05:07:18 +0100

    Update src/components/loading/fullscreenloading/index.tsx

--- src/components/Loading/FullscreenLoading/index.tsx
+++ src/components/Loading/FullscreenLoading/index.tsx
@@ -2,7 +2,6 @@ import { Center, Flexbox } from '@lobehub/ui';
 import { type ReactNode } from 'react';
 import React, { memo } from 'react';
 
-import { ProductLogo } from '@/components/Branding';
 import { type StageItem } from '@/components/InitProgress';
 import InitProgress from '@/components/InitProgress';
 
@@ -16,7 +15,17 @@ const FullscreenLoading = memo<FullscreenLoadingProps>(({ activeStage, stages, c
   return (
     <Flexbox height={'100%'} style={{ position: 'relative', userSelect: 'none' }} width={'100%'}>
       <Center flex={1} gap={16} width={'100%'}>
-        <ProductLogo size={48} type={'combine'} />
+        {/* 直接硬编码 Valo 品牌 */}
+        <Flexbox horizontal align="center" gap={8}>
+          <span style={{ fontSize: 48 }}>🌙</span>
+          <span style={{ 
+            fontWeight: 600, 
+            fontSize: 38,
+            lineHeight: 1
+          }}>
+            Valo
+          </span>
+        </Flexbox>
         {contentRender ? contentRender : <InitProgress activeStage={activeStage} stages={stages} />}
       </Center>
     </Flexbox>
