f5bb02d0e0a168520091b22e502439bd3890025a
Valko <gracederousy@gmail.com>
2026-03-14 06:55:59 +0100

    Update src/components/brandwatermark/index.tsx

--- src/components/BrandWatermark/index.tsx
+++ src/components/BrandWatermark/index.tsx
@@ -1,22 +1,14 @@
 'use client';
 
-import { ORG_NAME, UTM_SOURCE } from '@lobechat/business-const';
 import { type FlexboxProps } from '@lobehub/ui';
 import { Flexbox } from '@lobehub/ui';
-import { LobeHub } from '@lobehub/ui/brand';
 import { createStaticStyles, cssVar } from 'antd-style';
 import { memo } from 'react';
 
-import { isCustomORG } from '@/const/version';
-
-const styles = createStaticStyles(({ css, cssVar }) => ({
-  logoLink: css`
-    line-height: 1;
-    color: inherit;
-
-    &:hover {
-      color: ${cssVar.colorLink};
-    }
+const styles = createStaticStyles(({ css }) => ({
+  logoText: css`
+    font-weight: 600;
+    margin-left: 2px;
   `,
 }));
 
@@ -28,22 +20,11 @@ const BrandWatermark = memo<Omit<FlexboxProps, 'children'>>(({ style, ...rest })
       dir={'ltr'}
       flex={'none'}
       gap={4}
-      style={{ color: cssVar.colorTextDescription, fontSize: 12, ...style }}
+      style={{ color: cssVar.colorTextSecondary, fontSize: 12, ...style }}
       {...rest}
     >
       <span>Powered by</span>
-      {isCustomORG ? (
-        <span>{ORG_NAME}</span>
-      ) : (
-        <a
-          className={styles.logoLink}
-          href={`https://lobehub.com?utm_source=${UTM_SOURCE}&utm_content=brand_watermark`}
-          rel="noreferrer"
-          target="_blank"
-        >
-          <LobeHub size={20} type={'text'} />
-        </a>
-      )}
+      <span className={styles.logoText}>🌙 Valo</span>
     </Flexbox>
   );
 });
