cdc4347d453676cf3872cad13f0ea0beb5189caf
Valko <gracederousy@gmail.com>
2026-03-14 06:10:46 +0100

    Update src/components/branding/orgbrand/index.tsx

--- src/components/Branding/OrgBrand/index.tsx
+++ src/components/Branding/OrgBrand/index.tsx
@@ -1,14 +1,9 @@
-import { ORG_NAME } from '@lobechat/business-const';
-import { type LobeHubProps } from '@lobehub/ui/brand';
-import { LobeHub } from '@lobehub/ui/brand';
 import { memo } from 'react';
 
-import { isCustomORG } from '@/const/version';
-
-export const OrgBrand = memo<LobeHubProps>((props) => {
-  if (isCustomORG) {
-    return <span>{ORG_NAME}</span>;
-  }
-
-  return <LobeHub {...props} />;
+export const OrgBrand = memo(() => {
+  return (
+    <span style={{ fontWeight: 600 }}>
+      🌙 Valo
+    </span>
+  );
 });
